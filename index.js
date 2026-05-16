54f5f266b3e9503c159938c71c820c5bf7d5a6eb
TeamLead <isave35861@gmail.com>
2026-05-17 01:19:10 +0530

    for node js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Simple Node.js app
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Rajratna's Node.js app!");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
