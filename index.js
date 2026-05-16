f91da1a067239911bd7468654c3bb91535a07759
TeamLead <isave35861@gmail.com>
2026-05-17 01:53:41 +0530

    war

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Rajratna's Node.js app!");
+  res.end("Hello from Rajratna hano save'sNode.js app!");
 });
 
 const PORT = 3000;
